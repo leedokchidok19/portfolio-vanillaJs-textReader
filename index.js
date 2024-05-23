e935200164bc79c2afe2023139e2e2e95c41fcbb
leedokchidok19 <duckhyun4433@naver.com>
2024-05-23 20:51:41 +0900

    text reader

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// 정적 파일 서빙
+// 정적 파일을 제공하는 미들웨어 설정
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 포트 설정
-const PORT = process.env.PORT || 3000;
+// 모든 경로에 대해 index.html을 서빙
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
--- public/script.js
+++ public/script.js
@@ -1,1 +1,62 @@
-console.log('JavaScript is running!');
+/*
+top 버튼
+*/
+const HTML_HEIGHT = window.innerHeight;
+const TOP_BTN = document.querySelector('.topBtn');
+window.addEventListener('scroll', () => toggleTopBtn(TOP_BTN));
+
+function toggleTopBtn(topBtn){
+  const y = window.scrollY;
+  if(y >= HTML_HEIGHT) topBtn.classList.remove('d-none');
+  else topBtn.classList.add('d-none');
+}
+
+document.getElementById('fileInput').addEventListener('change', event => {
+
+  const file = event.target.files[0];
+  if (!file) return;
+
+  const reader = new FileReader();
+  reader.onload = function(event) {
+      const contents = event.target.result;
+      const lines = contents.split('\n');
+      displayContents(lines);
+  };
+  reader.readAsText(file);//기본 인코딩 UTF-8
+
+});
+
+function displayContents(lines) {
+
+  const contentDiv = document.getElementById('content');
+  // 초기화
+  contentDiv.innerHTML = '';
+  lines.forEach(function(line, index) {
+      const itemDiv = document.createElement('div');
+      itemDiv.classList.add('item');
+      itemDiv.innerHTML = ` <div>${index + 1}. ${line}</div>
+                            <textarea rows="4"></textarea>`;
+      contentDiv.appendChild(itemDiv);
+  });
+
+  document.getElementById('saveBtn').style.display = 'block';
+  document.getElementById('tempSave').style.display = 'block';
+
+}
+
+document.getElementById('saveBtn').addEventListener('click', function() {
+  const contents = [];
+  const items = document.querySelectorAll('.item');
+  items.forEach(function(item, index) {
+      const qnaIndex = index+1;
+      const question = item.querySelector('div').textContent;
+      const answer = item.querySelector('textarea').value;
+      contents.push(`질문${qnaIndex}: ${question}\n답변${qnaIndex}: ${answer}\n`);
+  });
+
+  saveTextFile(contents.join('\n'), 'QA.txt');
+
+});
+
+function saveTextFile(text, filename) {
+  const blob = new Blob([text], {
